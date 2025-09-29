c063aca5ea3cf6c1c19948ee0178d9d35524cfac
Robert Turner <rturner@catalyte.io>
2025-09-29 10:36:14 -0500

    created a class

---
+++ purchase.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+class Purchase:
+    class Purchase:
+        
+        def __init__(self, items, status, prices):
+            self.items = items
+            self.status = status
+            self.prices = prices
