29ed2064f37fcf1617460f0036892f1d456f7540
Robert Turner <rturner@catalyte.io>
2025-09-29 10:54:01 -0500

    add purchase id

--- purchase.py
+++ purchase.py
@@ -4,10 +4,11 @@ from zoneinfo import ZoneInfo
 class Purchase:
     class Purchase:
         
-        def __init__(self, items, status, prices):
+        def __init__(self, items, status, prices, purchase_id):
             self.items = items
             self.status = status
             self.prices = prices
+            self.purchase_id = purchase_id
 
         def date_and_time():
             chicago_time_now = datetime.now(tz=ZoneInfo("America/Chicago"))
