a8f5c03f9816a411cc4de5b811b19bfe8d21bf3b
Matthias Jones <mjones@catalyte.io>
2025-09-29 14:06:40 -0500

    Minor changes

--- app/models/ingredients.py
+++ app/models/ingredients.py
@@ -1,5 +1,13 @@
-class Ingredients:
-    def __init__(
+class Ingredient:
+     '''represents an ingredient with purchasing details
+     item_name (str): The name of the ingredient (e.g., "Flour").
+        purchasing_cost (float): The cost of purchasing the ingredient 
+            per unit_amount in the given unit_of_measure.
+        unit_amount (float): The quantity that the purchasing cost corresponds to.
+        unit_of_measure (str): The unit of measurement for the ingredient 
+            (e.g., "kg", "liter", "pack")'''.
+
+     def __init__(
         self, item_name, purchasing_cost, unit_amount, unit_of_measure, inventory
     ):
         self.item_name = item_name
@@ -7,3 +15,7 @@ class Ingredients:
         self.unit_amount = unit_amount
         self.unit_of_measure = unit_of_measure
         self.inventory = inventory
+
+        
+
+        
