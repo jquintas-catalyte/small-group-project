fce9554ef1fa56900ae2049d8465cc525861fc99
Matthias Jones <mjones@catalyte.io>
2025-09-29 14:17:09 -0500

    Minor updates

--- app/models/ingredients.py
+++ app/models/ingredients.py
@@ -1,21 +1,40 @@
-class Ingredient:
-     '''represents an ingredient with purchasing details
-     item_name (str): The name of the ingredient (e.g., "Flour").
-        purchasing_cost (float): The cost of purchasing the ingredient 
-            per unit_amount in the given unit_of_measure.
-        unit_amount (float): The quantity that the purchasing cost corresponds to.
-        unit_of_measure (str): The unit of measurement for the ingredient 
-            (e.g., "kg", "liter", "pack")'''.
+"""Definitions for Ingrediaent class"""
 
-     def __init__(
-        self, item_name, purchasing_cost, unit_amount, unit_of_measure, inventory
+
+class Ingredient:
+    """
+    Represents an ingredient.
+    Attributes:
+        item_name (str): The name of the ingredient (e.g., "Flour").
+        purchasing_cost (float): The cost of purchasing the ingredient per unit_amount.
+        unit_amount (float): The quantity that the purchasing cost corresponds to.
+        unit_of_measure (str): The unit of measurement for the ingredient.
+        category (str): The product category.
+    """
+
+    def __init__(
+        self,
+        item_name,
+        category,
+        purchasing_cost,
+        unit_amount,
+        unit_of_measure,
+        inventory,
     ):
+        """
+        Initializes the Ingredient class.
+
+        Parameters:
+            item_name (str): The name of the ingredient (e.g., "Flour").
+            category (str): The product category.
+            purchasing_cost (float): The cost of purchasing the ingredient per unit_amount.
+            unit_amount (float): The quantity of the product in the inventory.
+            unit_of_measure (str): The unit of measurement for the ingredient.
+        """
+
         self.item_name = item_name
+        self.category = category
         self.purchasing_cost = purchasing_cost
         self.unit_amount = unit_amount
         self.unit_of_measure = unit_of_measure
         self.inventory = inventory
-
-        
-
-        
