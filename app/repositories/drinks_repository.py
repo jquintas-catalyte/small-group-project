8f964ab703a426a47f077fba3b7b50fcc338848d
James Burns <jburns@catalyte.io>
2025-09-30 09:32:27 -0500

    Co-authored-by: Daniel Onibokun <donibokun@catalyte.io> Co-authored-by: Jashive Quintas <jquintas-catalyte@users.noreply.github.com>

---
+++ app/repositories/drinks_repository.py
@@ -0,0 +1,35 @@
+from models.drinks import Drink
+class DrinkRepository:
+    def __init__(self):
+        self._drinks = {}
+
+    def add(self, drink: Drink):
+        """Adds new drink to repository"""
+        if not isinstance(drink, Drink):
+            raise TypeError("Only Drink objects can be added to repository.")
+        if drink.name in self._drinks:
+            raise ValueError(f"Drink with name {drink.name} already exists")
+        self.drinks[drink.name] =drink
+        print(f"Added drink: {drink.name}")
+
+    def get_my_name(self, name: str):
+        """Retrieves a drink by name."""
+        return self.drinks.get(name)
+    
+    def get_all(self):
+        """Returns a list of all drinks in repository."""
+        return list(self.drinks.values())
+    
+    def update(self, drink: Drink):
+        """Updates an existing drink in the repository."""
+        if drink.name not in self.drinks:
+            raise ValueError(f"Drink with name {drink.name} not found.")
+        self._drinks[drink.name] = drink
+        print(f"Updated drink: {drink.name}")
+
+    def delete(self, name: str):
+        """Deletes a drink from within the repository by name"""
+        if name not in  self._drinks:
+            raise ValueError(f"Drink with name {name} not found.")
+        del self.drinks[name]
+        print(f"Deleted drink: {name}")
