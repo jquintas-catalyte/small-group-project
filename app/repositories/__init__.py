36a9782e308be3c4f7d8e0d9701913978758b5be
Daniel Onibokun <idanieloni@gmail.com>
2025-09-30 11:21:15 -0500

    Minor updates

--- app/repositories/__init__.py
+++ app/repositories/__init__.py
@@ -1,5 +1,4 @@
 import os
 root_data_path = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'data')
-
 from .baked_goods_repository import BakedGoodsRepository
 from .purchases_repository import PurchasesRepository
